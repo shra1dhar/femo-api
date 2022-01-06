aefd8c20bdff3fe8dfdb63b8d49424436d5c5123
shra1dhar <shraonedhar@gmail.com>
2022-01-06 23:58:29 +0530

    Bulletin

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
+	"time"
 )
+
+// https://medium.com/geekculture/work-with-go-postgresql-using-pgx-caee4573672
 
 const (
 	DbHost = "db"
 	DbUser = "postgres-dev"
 	DbPassword = "mysecretpassword"
 	Dbname  = "dev"
+	Migration = `CREATE TABLE IF NOT EXISTS bulletins
+id serial PRIMARY KEY
+author TEXT NOT NULL
+content TEXT NOT NULL
+created_at timestamp with timezone DEFAULT current_timestamp`
 )
+
+type Bulletin struct {
+	Author string `json:"author" binding:"required"`
+	Content string `json:"content" binding:"required"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+var db *sql.DB
+
+func GetBulletins() ([]Bulletin, error) {
+	return nil, nil
+}
+
+func AddBulletin(bulletin Bulletin) error {
+	return nil
+}
 
 func main() {
 	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))

c8e68165c18c18f5f09cb1f6a2eaee685b82ddb0
shra1dhar <shraonedhar@gmail.com>
2022-01-08 23:46:20 +0530

    Dockerise the application

--- main.go
+++ main.go
@@ -1,83 +1,16 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"net/http"
-	"os"
-	"time"
+	"github.com/gin-gonic/gin"
 )
-
-// https://medium.com/geekculture/work-with-go-postgresql-using-pgx-caee4573672
-
-const (
-	DbHost     = "db"
-	DbUser     = "postgres-dev"
-	DbPassword = "mysecretpassword"
-	Dbname     = "dev"
-	Migration  = `CREATE TABLE IF NOT EXISTS bulletins
-id serial PRIMARY KEY
-author TEXT NOT NULL
-content TEXT NOT NULL
-created_at timestamp with timezone DEFAULT current_timestamp`
-)
-
-type Bulletin struct {
-	Author    string    `json:"author" binding:"required"`
-	Content   string    `json:"content" binding:"required"`
-	CreatedAt time.Time `json:"createdAt"`
-}
-
-var db *sql.DB
-
-func GetBulletins() ([]Bulletin, error) {
-	return nil, nil
-}
-
-func AddBulletin(bulletin Bulletin) error {
-	return nil
-}
 
 func main() {
-	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer dbpool.Close()
-
-	var greeting string
-	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
 
-	r.POST("/form_post", func(c *gin.Context) {
-		message := c.PostForm("message")
-		nick := c.DefaultPostForm("nick", "anonymous")
-
-		c.JSON(http.StatusOK, gin.H{
-			"status":  "sent",
-			"message": message,
-			"nick":    nick,
-		})
-	})
-
-	err = r.Run()
-	if err != nil {
-		fmt.Println("Cannot run server. App crashed")
-	}
-
-	fmt.Println(greeting)
+	r.Run()
 }
